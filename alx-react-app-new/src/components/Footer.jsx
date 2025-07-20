bba7c2c9c25b450a0dc8fa81b89e5086718fec3f
MAHIR468 <mohamedmahir468@gmail.com>
2025-07-20 16:28:05 +0100

    Create Footer.jsx

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'gray', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>Copyright &copy; 2025</p>
+    </footer>
+  );
+}
